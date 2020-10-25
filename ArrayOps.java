bdbd11063ace3b47e8d498c257b93b13a14b661f
dib1000 <dib5017@gmail.com>
2020-10-25 14:20:51 -0400

    I tested the sum method for 2D arrays and it works

--- ArrayOps.java
+++ ArrayOps.java
@@ -29,4 +29,11 @@ public class ArrayOps {
     }
     return largeRow;
   }
+  public static  int sum(int[][] arr) {
+    int arrSum = 0;
+    for(int i = 0; i<arr.length; i++) {
+      arrSum += sum(arr[i]);
+    }
+    return arrSum;
+  }
 }
--- Tester.java
+++ Tester.java
@@ -4,6 +4,7 @@ public class Tester {
     int[] B = { 55,54,-32,46 };
     int[][] A = { {  1,  0, 12, -1 }, {  7, -2,  2,  1 }, { -5, -2,  2, -9 } };
     System.out.println(ArrayOps.sum(B));
+    System.out.println(ArrayOps.sum(A));
     System.out.println(ArrayOps.largest(B));
     System.out.println( Arrays.toString(ArrayOps.sumRows(A)));
     System.out.println( Arrays.toString(ArrayOps.largestInRows(A)));
